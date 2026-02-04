69f4aeda6e82737fb1b6b87d6674503419b36e3e
FinnMcGowan <fmcgowan1@babson.edu>
2026-02-04 13:35:49 -0500

    boat flight mod template upload

---
+++ code/minecraft_mods/boat_flight/boatflight-template-1.21.10/src/client/java/boatflight/BoatflightClient.java
@@ -0,0 +1,10 @@
+package boatflight;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BoatflightClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
